e98fd5c8ce70ae957aa97906314a9d81fa7bde36
Suraj Mallick <surajhustlermallick@gmail.com>
2023-09-07 19:11:10 +0530

    built secondary container with movie list and created custom hook for popular movies and improved css for container component

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,8 +1,43 @@
-import React from 'react'
+import React from 'react';
+import MovieList from './MovieList';
+import { useSelector } from 'react-redux';
 
 const SecondaryContainer = () => {
+
+  const movies=useSelector(store => store.movies);
+
+
   return (
-    <div>SecondaryContainer</div>
+
+    movies.nowPlayingMovies && 
+
+    <div className=" bg-black">
+
+      <div className="-mt-52 relative z-20 pl-12">
+      <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
+
+      <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
+
+      <MovieList title={"Popular"} movies={movies.popularVideo} />
+
+      <MovieList title={"Upcoming Movies"} movies={movies.nowPlayingMovies} />
+
+      <MovieList title={"Horror Movies"} movies={movies.nowPlayingMovies} />
+      </div>
+
+      
+      {
+        /*
+        Movie List -popular
+        Movie List -Now Playing
+        Movie List -Trending Now
+        Movie List -Romantic
+        Movie List -Documentaries
+        Movie List -Now Playing
+
+ 
+      */ }
+    </div>
   )
 }
 
--- src/utils/moviesSlice.js
+++ src/utils/moviesSlice.js
@@ -4,7 +4,8 @@ const moviesSlice = createSlice({
     name:"movies",
     initialState:{
         nowPlayingMovies:null,
-        trailerVideo:null
+        trailerVideo:null,
+        popularVideo:null
     },
     reducers:{
         addNowPlayingMovies:(state,action)=>{
@@ -12,11 +13,14 @@ const moviesSlice = createSlice({
         },
         addTrailerVideo:(state,action) => {
             state.trailerVideo=action.payload
+        },
+        addPopularMovies:(state,action) => {
+            state.popularVideo=action.payload
         }
 
     }
 })
 
-export const { addNowPlayingMovies , addTrailerVideo }=moviesSlice.actions;
+export const { addNowPlayingMovies , addTrailerVideo ,addPopularMovies}=moviesSlice.actions;
 
 export default moviesSlice.reducer;
