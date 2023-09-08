f32b484309379cca9c8967dee7327b7a3bfd1d0e
Suraj Mallick <surajhustlermallick@gmail.com>
2023-09-08 22:24:34 +0530

    Made the GPT Button Toggle and also made the Select Lang Button Hide when you are in Home Page and not in GPT Search Page

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,6 +19,8 @@ const Header = () => {
 
   const user=useSelector(store => store.user);
 
+  const showGptSearch=useSelector((store)=>store.gpt.showGptSearch)
+
   const handleSignOut=()=>{
     signOut(auth).then(() => {
       //navigate("/")
@@ -73,12 +75,14 @@ const Header = () => {
         {user && 
           <div className="flex p-2">
 
-            <select className="p-2 m-2 bg-gray-900 text-white" onChange={handleLanguageChange}>
+            { showGptSearch &&
+              <select className="p-2 m-2 bg-gray-900 text-white" onChange={handleLanguageChange}>
               {SUPPORTED_LANGUAGES.map(lang => <option key={lang.identifier} value={lang.identifier}>{lang.name}</option>)}
             </select>
+            }
 
           <button className="py-2 px-4 mx-5 my-2 bg-purple-800 text-white rounded-lg"
-          onClick={handleGptSearchClick}>GPT Search</button>
+          onClick={handleGptSearchClick}>{showGptSearch ?"Home Pag" : "GPT Search"}</button>
 
           <img className="w-12 h-12 " alt="user-icon" src={user?.photoURL}/>
 
